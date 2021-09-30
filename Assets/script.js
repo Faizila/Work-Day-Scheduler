4ba7cb57a6972aa28446ccc429a8128114c1c61f
Faizila Amlani <amlani.faizila@gmail.com>
2021-09-30 10:29:38 -0500

    updated loop in script.js

--- Assets/script.js
+++ Assets/script.js
@@ -3,53 +3,53 @@ $(document).ready(function () {
   var todayDate = moment().format("dddd, MMM Do YYYY LT");
   $("#currentDay").html(todayDate);
 
-  // Save Button event listener
-  $(".saveBtn").on("click", function () {
-    //get nearby values
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+    // save button eventListener
+    $(".saveBtn").on("click", function () {
+        // Get nearby values
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-    //save text in local storage
-    localStorage.setItem(time, text);
-  });
-
-  //retrieve saved text from LocalStorage
-  $("#nine .description").val(localStorage.getItem("nine"));
-  $("#ten .description").val(localStorage.getItem("ten"));
-  $("#eleven .description").val(localStorage.getItem("eleven"));
-  $("#twelve .description").val(localStorage.getItem("twelve"));
-  $("#one .description").val(localStorage.getItem("one"));
-  $("#two .description").val(localStorage.getItem("two"));
-  $("#three .description").val(localStorage.getItem("three"));
-  $("#four .description").val(localStorage.getItem("four"));
-  $("#five .description").val(localStorage.getItem("five"));
-
-  function timeTracker() {
-    //get current hour
-    var currentHour = moment().hour();
-
-    // loop over time blocks
-    $(".time-block").each(function () {
-      var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-
-      //compare time
-      if (timeBlock < currentHour) {
-        $(this).addClass("past");
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-      } 
-      else if (timeBlock === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-        $(this).removeClass("future");
-      } 
-      else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-      }
+        // Save value in local storage
+        localStorage.setItem(time, value);
     });
-  }
-  // re-run function
-  timeTracker();
+   
+    function timeTracker() {
+        //get the current hour
+        var currentHour = moment().hour();
+
+        //Loop
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+            //Add classes for color indicator
+            if (timeBlock < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");               
+            } else if (timeBlock > currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
+    timeTracker(); 
+
+    var interval = setInterval(timeTracker, 15000);
+  
+ // Get item from local storage
+ $("#nine .description").val(localStorage.getItem("nine"));
+ $("#ten .description").val(localStorage.getItem("ten"));
+ $("#eleven .description").val(localStorage.getItem("eleven"));
+ $("#twelve .description").val(localStorage.getItem("twelve"));
+ $("#one .description").val(localStorage.getItem("one"));
+ $("#two .description").val(localStorage.getItem("two"));
+ $("#three .description").val(localStorage.getItem("three"));
+ $("#four .description").val(localStorage.getItem("four"));
+ $("#five .description").val(localStorage.getItem("five"));
+
 });
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Work-Day-Scheduler
 
-* A simple calendar application that allows a user to save events for each hour of the day.
+* A simple calendar application that allows a user to save events for each working hour of the day.
 
 # Description
 
@@ -8,9 +8,11 @@
 
 * Top center of the page displays current day, date and time.
 
-*Each time block is color coded to indicate past(gray), present(red), and future(green) time.
+* User can save events for each working hour of the day.
 
-*Each time block has a text area and a save button next to it, where users can save their schedule for the day.
+* Each time block has a text area and a save button next to it, where users can save their schedule for the day.
+
+*Each time block is color coded to indicate whether it is in the past(gray), present(red), or future(green).
 
 * Once user enters the information and clicks the save button, the information will get stored in their local storage.
 
@@ -23,6 +25,7 @@
 * Moment.js
 * Bootstrap
 * Google Fonts
+* Font Awesome
 
 ## Link of Deployed Application
 
